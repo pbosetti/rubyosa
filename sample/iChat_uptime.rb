d9a2bf2015ef8ca14e0b0f13385e7e91b45a1dd8
Laurent Sansonetti <lsansonetti@apple.com>
2006-10-27 00:47:06 +0000

    2006-10-27  Laurent Sansonetti  <lsansonetti@apple.com>
        * sample/iChat_uptime.rb:
        Better uptime string. Suggested by Tieg Zaharia.
    
    
    git-svn-id: http://rubyosa.rubyforge.org/svn/trunk@97 0d7f026f-1217-0410-b43a-e108cceea134

--- sample/iChat_uptime.rb
+++ sample/iChat_uptime.rb
@@ -5,7 +5,10 @@ require 'rbosa'
 app = OSA.app('iChat')
 previous_status_message = app.status_message
 trap('INT') { app.status_message = previous_status_message; exit 0 }
-while true 
-    app.status_message = `uptime`.strip 
+while true
+    u = `uptime`
+    hours = u.scan(/^\s*(\d+:\d+)\s/).to_s + ' hours'
+    days = u.scan(/\d+\sdays/).to_s
+    app.status_message = "OSX up #{days} #{hours}"
     sleep 5
 end

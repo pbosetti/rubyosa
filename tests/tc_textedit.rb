be31ae46eebdab56dfcd806e387af922e4fea2b0
Laurent Sansonetti <lsansonetti@apple.com>
2007-01-29 19:27:05 +0000

    close all opened documents before starting the tests
    git-svn-id: http://rubyosa.rubyforge.org/svn/trunk@150 0d7f026f-1217-0410-b43a-e108cceea134

--- tests/tc_textedit.rb
+++ tests/tc_textedit.rb
@@ -32,6 +32,7 @@ require 'fileutils'
 class TC_TextEdit < Test::Unit::TestCase
   def setup
     @textedit = OSA.app_with_name('TextEdit')
+    @textedit.documents.each { |x| x.close }
   end
 
   def do_as(str)

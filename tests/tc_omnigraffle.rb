3b8e3a81b51cfe29ecb5389d9b4885ce4e583d4d
Laurent Sansonetti <lsansonetti@apple.com>
2007-02-05 18:17:47 +0000

    2007-02-05  Laurent Sansonetti  <lsansonetti@apple.com>
      * tests/tc_omnigraffle.rb,
        tests/data/test.graffle:
      Added a test case to verify the last fix.
    
    
    git-svn-id: http://rubyosa.rubyforge.org/svn/trunk@165 0d7f026f-1217-0410-b43a-e108cceea134

---
+++ tests/tc_omnigraffle.rb
@@ -0,0 +1,47 @@
+# Copyright (c) 2006-2007, Apple Inc. All rights reserved.
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+# 1.  Redistributions of source code must retain the above copyright
+#     notice, this list of conditions and the following disclaimer. 
+# 2.  Redistributions in binary form must reproduce the above copyright
+#     notice, this list of conditions and the following disclaimer in the
+#     documentation and/or other materials provided with the distribution. 
+# 3.  Neither the name of Apple Inc. ("Apple") nor the names of
+#     its contributors may be used to endorse or promote products derived
+#     from this software without specific prior written permission. 
+# 
+# THIS SOFTWARE IS PROVIDED BY APPLE AND ITS CONTRIBUTORS "AS IS" AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL APPLE OR ITS CONTRIBUTORS BE LIABLE FOR
+# ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+# OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+# STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING
+# IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+require 'test/unit'
+require 'rbosa'
+
+class TC_OmniGraffle < Test::Unit::TestCase
+  def setup
+    @omnigraffle = OSA.app('OmniGraffle Professional')
+  end
+
+  def test_smart_get
+    begin
+      @omnigraffle.open(File.join(Dir.pwd, 'data/test.graffle'))
+      obj = @omnigraffle.documents[0].canvases[0].groups[0]
+      assert_kind_of(OSA::OmniGraffleProfessional::Group, obj)
+      resolved_obj = obj.get
+      assert_kind_of(OSA::OmniGraffleProfessional::Table, resolved_obj)
+    ensure
+      @omnigraffle.documents.each { |d| d.close }
+    end
+  end
+
+end

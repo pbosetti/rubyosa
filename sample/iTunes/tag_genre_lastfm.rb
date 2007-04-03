22d16ece67774482daa61c570525de931668b319
Laurent Sansonetti <lsansonetti@apple.com>
2007-04-03 13:24:09 +0000

    2007-04-03  Wes Rogers  <wesrog@gmail.com>
      * sample/iTunes/tag_genre_lastfm.rb:
      New revision, that will only query Last.fm once per artist instead of multiple
      times (to not abuse the webservice they provide). 
    
    
    git-svn-id: http://rubyosa.rubyforge.org/svn/trunk@203 0d7f026f-1217-0410-b43a-e108cceea134

--- sample/iTunes/tag_genre_lastfm.rb
+++ sample/iTunes/tag_genre_lastfm.rb
@@ -4,17 +4,29 @@ begin require 'rubygems'; rescue LoadError; end
 require 'rbosa'
 require 'net/http'
 require 'cgi'
-require 'rexml/document'
+require 'rexml/document' 
 include REXML
 
 itunes = OSA.app('iTunes')
+
 selection = itunes.selection.get
 if selection.empty?
   $stderr.puts "Please select some tracks."
   exit 1
 end
+
+first = selection.first.artist 
+feed = "http://ws.audioscrobbler.com/1.0/artist/#{CGI::escape(first)}/toptags.xml"
+doc = Document.new(Net::HTTP.get(URI(feed))) 
+
 selection.each do |track|
-  feed = "http://ws.audioscrobbler.com/1.0/artist/#{CGI::escape(track.artist)}/toptags.xml"
-  doc = Document.new(Net::HTTP.get(URI(feed)))
-  track.genre = doc.root[1][1].text
+  if doc.root.attributes['artist'] == track.artist
+    genre = doc.root[1][1].text.capitalize
+  else
+    puts 'Querying Last.fm again...' 
+    feed = "http://ws.audioscrobbler.com/1.0/artist/#{CGI::escape(track.artist)}/toptags.xml"
+    doc = Document.new(Net::HTTP.get(URI(feed)))
+    genre = doc.root[1][1].text.capitalize
+  end
+  track.genre = genre
 end
